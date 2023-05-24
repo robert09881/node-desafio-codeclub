2c32ee811cfce2bb2e9f9d2ac42faba8b138aa14
robert09881 <roberto.sert88@gmail.com>
2023-05-24 14:34:23 -0300

    Desafio Pronto Node

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const orders = []
 const checkOrderId = (request, response, next) => {
     const { id } = request.params
     
-    const index = orders.findIndex(purchaseOrder => purchaseOrder.id === id)
+    const index = orders.findIndex(purchaseOrder => purchaseOrder.id == id)
     if (index < 0) {
         return response.status(404).json({ error: "order not found"})
     } 
@@ -24,7 +24,7 @@ const checkOrderId = (request, response, next) => {
 const checkUrl = (request, response, next) => {
     const method = request.method
     const url = request.url
-    console.log(`method used: ${method}, and Url used: ${url}`)
+    console.log(`${method}, ${url}`)
 
     next()
 }
@@ -32,7 +32,7 @@ const checkUrl = (request, response, next) => {
 
 
 
-app.get('/orders', (request, response) => {
+app.get('/orders', checkUrl, (request, response) => {
 
 
 
@@ -41,7 +41,7 @@ app.get('/orders', (request, response) => {
 
 
 
-app.post('/orders', (request, response) => {
+app.post('/orders', checkUrl, (request, response) => {
     const { order, ClientName, Price } = request.body
 
     const purchaseOrder = { id: uuid.v4(), order, ClientName, Price, status: "Em preparação" }
@@ -53,36 +53,34 @@ app.post('/orders', (request, response) => {
     return response.status(201).json(purchaseOrder)
 })
 
-app.put('/orders/:id', checkOrderId, (request, response) => {
+app.put('/orders/:id', checkOrderId, checkUrl, (request, response) => {
 
-    const { id } = request.params
-    const { ClientName, order, Price } = request.body
-
-    const updatedOrder = { id, ClientName, order, Price, status: "Em preparação" }
-
+    const { order, ClientName, Price } = request.body
     const index = request.orderIndex
+    const id = request.orderId
+    const status = orders[index].status
 
-
-
-    if (index < 0) {
-        return response.status(404).json({ message: "Order not found" })
-    }
+    const updatedOrder = { id, order, ClientName, Price, status }
+   //if (index < 0) {
+      //  return response.status(404).json({ message: "Order not found" })
+   // }
 
     orders[index] = updatedOrder
 
 
     return response.json(updatedOrder)
-})
+});
 
-app.delete('/orders/:id', checkOrderId, (request, response) => {
+app.delete('/orders/:id', checkOrderId, checkUrl, (request, response) => {
     const index = request.orderIndex
 
     orders.splice(index, 1)
 
     return response.status(204).json()
-})
+});
 
-app.patch('/orders/:id', checkOrderId, (request, response) => {
+app.patch('/orders/:id', checkOrderId, checkUrl, (request, response) => {
+    
     const index = request.orderIndex
     const purchaseOrder = orders[index]
 
@@ -93,7 +91,7 @@ app.patch('/orders/:id', checkOrderId, (request, response) => {
     return response.json(purchaseOrder)
 })
 
-app.get('/orders/:id', checkOrderId, (request, response) =>{
+app.get('/orders/:id', checkOrderId, checkUrl, (request, response) =>{
     const index = request.orderIndex
     const purchaseOrder = orders[index]
 
